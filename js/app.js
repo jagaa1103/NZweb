a377635ff67b6654ca68c72be84ed9d84c7c3e41
Jagaa <jagaa1103@gmail.com>
2015-06-05 16:27:28 +0900

    changed UI

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-var app = angular.module('nz', []);
+var app = angular.module('nz', ['ui.bootstrap', 'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  
+  $urlRouterProvider.otherwise("/main");
+  
+  $stateProvider
+    .state('main', {
+      url: "/main",
+      templateUrl: "views/main.html"
+    })
+    // .state('state1.list', {
+    //   url: "/list",
+    //   templateUrl: "partials/state1.list.html",
+    //   controller: function($scope) {
+    //     $scope.items = ["A", "List", "Of", "Items"];
+    //   }
+    // })
+    // .state('state2', {
+    //   url: "/state2",
+    //   templateUrl: "partials/state2.html"
+    // })
+;
+});
+
